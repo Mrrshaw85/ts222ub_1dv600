b9e54c7c7b18f71ed5cd765938acc71e985dc02c
Thimmy Stenlund <ts222ub@student.lnu.se>
2019-02-08 11:03:18 +0100

    Added skeleton code and folder for documents.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ts222ub_1dv600
 Student repository for 1dv600
 
-Student ID: ts222ub
-Student Full Name: Thimmy Stenlund
+Student ID: ts222ub  
+Student Full Name: Thimmy Stenlund  
+
+Hang The Man - The Game  
+  
+See the "app.js" for the skeleton code.
---
+++ app.js
@@ -0,0 +1,20 @@
+/* Hang The Man - The Game
+
+// How to play the game (Basic)
+* Random word is selected - not visible to player
+* Number of _ for each letter in the word is displayed
+* Choose a letter
+* If letter is part of the random word - reveal it at its location _
+* If letter is not part of the random word - display it as "wrong"
+* Display each status towards a man being hung for each wrong guess of a letter in the random word.
+* If all letters are correctly guessed - display that the player has won
+* If all steps towards hanging the man has been done, the game is over.
+* Display "Game Over" in the console.
+
+Features to be done:
+- Optional game mode where the player is supposed to hang the man.
+- Number of guessed letters displayed on screen (console) when you guess the correct word.
+- Time it took to guess the right word.
+- Highscore list that display the 10 fastest winners (including the number of guesses)
+
+*/
